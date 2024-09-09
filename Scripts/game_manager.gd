43e3a3bb4ee7b3dc2f0dba63b4b081f3a1c26e79
Lucas Souza <outrolucas2002@gmail.com>
2024-09-09 11:49:01 -0300

    Update debug game manager label of finished weapons

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -5,12 +5,15 @@ var string_list: String = ""
 
 @onready var weapons_list: Label = $WeaponsList
 
-func sell_weapon(weapon: String) -> void:
+func sell_weapon(weapon) -> void:
 	weapons_sold.append(weapon)
 
-	string_list = "Weapons sold: \n"
+	string_list = "Weapons sold: \n\n"
 
 	for weapon_item in weapons_sold:
-		string_list += "- " + weapon_item + "\n"
+		string_list += "- " + weapon_item['name'] + "\n"
+		string_list += "- - Forge Level: " + str(weapon_item['forge_level']) + "\n"
+		string_list += "- - Anvil Level: " + str(weapon_item['anvil_level']) + "\n"
+		string_list += "- - Whetstone Level: " + str(weapon_item['whetstone_level']) + "\n\n"
 
 	weapons_list.text = string_list
