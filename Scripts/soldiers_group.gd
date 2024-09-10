c3255ee60678047328f51f1cfad8f4d4e9d9c064
Lucas Felipe <outrolucas2002@gmail.com>
2024-09-10 15:53:31 -0300

    Start new soldier logic

--- Scripts/soldiers_group.gd
+++ Scripts/soldiers_group.gd
@@ -1,8 +1,6 @@
 extends Node2D
 
 var soldier_scene = preload("res://Scenes/soldier.tscn")
-
-var soldiers_count: int = 0
 var weapons_sold: Array = []
 
 @export var boundary_up: int = -42
@@ -12,27 +10,14 @@ func _ready() -> void:
 	pass
 
 func _process(_delta: float) -> void:
-	soldiers_count = get_child_count()
+	pass
 
 func sell_weapon(weapon) -> void:
 	weapons_sold.append(weapon)
-	soldiers_count += 1
 
 	var new_soldier = soldier_scene.instantiate()
-	new_soldier.name = "Soldier " + str(soldiers_count)
+	new_soldier.name = "Soldier " + str(get_child_count() + 1)
 	new_soldier.position = Vector2(35, 90)
-	new_soldier.weapon = weapon
 	add_child(new_soldier)
 
-	for i in range(get_child_count()):
-		var soldier = get_child(i)
-
-		if i != get_child_count() - 1:
-			soldier.move_randomly()
-
-func get_soldier_number(soldier) -> int:
-	for i in range(get_child_count()):
-		if get_child(i) == soldier:
-			return i
-
-	return -1
+	new_soldier.move_to_position(Vector2(35, 0))
