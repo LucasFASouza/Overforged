dcf0dd7fecc6bc333e8ebad9d95129168872df92
Lucas Souza <outrolucas2002@gmail.com>
2024-09-11 12:21:46 -0300

    Change anvil minigame from collider to position based

--- Scripts/anvil_minigame.gd
+++ Scripts/anvil_minigame.gd
@@ -5,32 +5,43 @@ var direction = -1
 var is_stopped = false
 
 @onready var hammer: CharacterBody2D = $Hammer
-var is_sweetspot = false
+var hammer_min
+var hammer_max
+
+@onready var sweetspot: Node2D = $Sweetspot
+var sweetspot_min
+var sweetspot_max
 
 
 func _process(delta):
 	if not is_stopped:
 		hammer.position += Vector2(speed * delta * direction * -1, 0)
 	else:
-		print("Victory? ", is_sweetspot)
+		# TODO: This is not working, it's getting the relative position, instead of abslute
+		sweetspot_min = sweetspot.get_node("MinX").position.x
+		sweetspot_max = sweetspot.get_node("MaxX").position.x
+		
+		hammer_min = hammer.get_node("MinX").position.x
+		hammer_max = hammer.get_node("MaxX").position.x
+
+		# Check if hammer is inside sweetspot
+		print("The sweet spot was: ", sweetspot_min, sweetspot_max)
+		print("The hammer was: ", hammer_min, hammer_max)
+		
+		if hammer_min >= sweetspot_min and hammer_max <= sweetspot_max:
+			print("Max Success - 3 Stars")
+		elif hammer_min >= sweetspot_min or hammer_max <= sweetspot_max:
+			print("Not Bad - 2 Stars")
+		else:
+			print("Fail - 1 Star")
 	
 	if Input.is_action_just_pressed("ui_select"):
 		is_stopped = true
 
-func _on_sweetspot_body_entered(body: Node2D) -> void:
-	is_sweetspot = true
-	print("Sweetspot entered")
-
-func _on_sweetspot_body_exited(body: Node2D) -> void:
-	is_sweetspot = false
-	print("Sweetspot exited")
-
-func _on_min_x_body_entered(body: Node2D) -> void:
-	print("Bateu")
+func _on_min_x_body_entered(_body: Node2D) -> void:
 	direction = -1
 
-func _on_max_x_body_entered(body: Node2D) -> void:
-	print("Bateu")
+func _on_max_x_body_entered(_body: Node2D) -> void:
 	direction = 1
 
 # TODO:
