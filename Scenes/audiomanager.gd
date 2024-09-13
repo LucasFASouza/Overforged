c6b39d5584f62ce1c815c672b1d4c76477551e8e
Taurisaurus <tauripunder@gmail.com>
2024-09-13 17:48:02 +0300

    Updated music and added function for switching music

--- Scenes/audiomanager.gd
+++ Scenes/audiomanager.gd
@@ -1,9 +1,11 @@
 extends Node
 
 var sfx_dict = {}
-
+@onready var main_music: AudioStreamPlayer2D = $Music/MainMusic
+@onready var battle_music: AudioStreamPlayer2D = $Music/BattleMusi
 
 func _ready() -> void:
+	main_music.play()
 	var sfx_node = $SFX
 	for child in sfx_node.get_children():
 		if child is AudioStreamPlayer2D: 
@@ -14,3 +16,13 @@ func play_sfx(sfx_name: String) -> void:
 		sfx_dict[sfx_name].play()
 	else:
 		print("SFX: %s not found!" % sfx_name)
+ 
+func switch_music(to_battle: bool):
+	if to_battle:
+		if main_music.playing:
+			main_music.stop()
+		battle_music.play()
+	else:
+		if battle_music.playing:
+			battle_music.stop()
+		main_music.play()
