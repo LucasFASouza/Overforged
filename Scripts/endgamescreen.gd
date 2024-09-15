77e07af3037f873d53c3b726000a8ba4c9ff9a99
Lucas Felipe <outrolucas2002@gmail.com>
2024-09-15 05:33:29 -0300

    Fix try again button

--- Scripts/endgamescreen.gd
+++ Scripts/endgamescreen.gd
@@ -1,16 +1,13 @@
 extends Control
 
-var game_scene = preload("res://Scenes/world.tscn")
 @onready var game_over_text_label: Label = $MarginContainer/VBoxContainer/MenuContainer/MarginContainer/PanelContainer/Main/GameOverTextLabel
 @onready var result_text_label: Label = $MarginContainer/VBoxContainer/MenuContainer/MarginContainer/PanelContainer/Main/ResultTextLabel
 
 
 
 func _on_try_again_button_pressed() -> void:
-	var new_scene = game_scene.instantiate()
-	get_tree().root.add_child(new_scene)
-	get_tree().current_scene.queue_free() 
-	get_tree().current_scene = new_scene 
+	get_tree().paused = false
+	get_tree().reload_current_scene()
 
 
 func _on_quit_button_pressed() -> void:
